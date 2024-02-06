b246665d1de9dba20f139eacca37ba096e01be70
Mateusz <alirymc@gmail.com>
2024-02-06 12:10:19 +0100

    Width cart photo

--- src/app/shop/page.js
+++ src/app/shop/page.js
@@ -6,7 +6,10 @@ import { CartProvider } from '../contexts/CartContext'
 import Menu from '../components/shopmenu'
 import products from "../components/products"
 export default function Shop(){
-    console.log(products)
+   const show = ()=>{
+    let one = document.querySelector(`.${style.ullist}`)
+    one.classList.toggle(`${style.show}`)
+   }
     return(
         <CartProvider>
                     <div id={style.page}>
@@ -14,7 +17,12 @@ export default function Shop(){
                         <Slider/>
                         <div id={style.oferts}>
                             <div id={style.select}>
-                                <div className={style.divbutton}><button className={style.button}>Descending</button><span className={style.topicleft}>Price</span></div>
+                                <div className={style.divbutton}><button onClick={()=>show()} className={style.button}>Descending</button><span className={style.topicleft}>Price</span>
+                                    <ul className={`${style.ullist} ${style.show}`}>
+                                        <li className={style.lilist}>Ascending</li>
+                                        <li className={style.lilist}>Descending</li>
+                                    </ul>
+                                </div>
                                 <div className={style.divbutton}><button className={style.button}>Sneakers</button><span className={style.topicright}>Category</span></div>
                             </div>
                             <div id={style.carts}>
